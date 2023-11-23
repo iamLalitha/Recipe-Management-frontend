78a0f1bf7d0b25f527d58964660b3e4117b4b4ac
Lalitha <130632883+iamLalitha@users.noreply.github.com>
2023-11-23 15:10:38 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,7 +45,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link className="navbar-brand" to="/" style={{color:'white'}}>RecipeFinder</Link>
+        <Link className="navbar-brand" to="/recipes" style={{color:'white'}}>RecipeFinder</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto">
           <li className="nav-item">
